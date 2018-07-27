13876f4ecfdda04adda0912bcc532e417071d24c
dmcbrayer <dmcbrayer@gmail.com>
2018-07-27 16:42:10 -0400

    Implements the creation of a new IAM user with S3 permissions.

--- main.tf
+++ main.tf
@@ -1,3 +1,42 @@
 provider "aws" {
   profile = "${var.aws_profile}"
 }
+
+resource "aws_iam_user" "s3-user" {
+  name = "${var.iam_user_name}"
+}
+
+resource "aws_iam_access_key" "s3-user" {
+  user = "${aws_iam_user.s3-user.name}"
+}
+
+resource "aws_iam_user_policy" "s3-policy" {
+  name = "s3-policy"
+  user = "${aws_iam_user.s3-user.name}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "Stmt1532723878817",
+      "Action": "s3:*",
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+output "user_name" {
+  value = "${aws_iam_user.s3-user.name}"
+}
+
+output "key_id" {
+  value = "${aws_iam_access_key.s3-user.id}"
+}
+
+output "secret" {
+  value = "${aws_iam_access_key.s3-user.secret}"
+}
--- variables.tf
+++ variables.tf
@@ -2,3 +2,13 @@ variable "aws_profile" {
   description = "The aws profile name set up in your environment"
   default = "roundtable"
 }
+
+/*
+  variable "bucket_name" {
+    description = "What you want your bucket to be called"
+  }
+*/
+
+variable "iam_user_name" {
+  description = "What you want your IAM user role to be called"
+}
